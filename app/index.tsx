b65743184c2c9201c477594309f84c341c105bb0
Jacson Souza <111143365+jacsonsouza@users.noreply.github.com>
2023-03-21 21:58:32 -0300

    Expo Router setup (#2)

---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import { Link, useRouter } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const router = useRouter();
+
+  const linkList = () => {
+    router.push({
+      pathname: "/list"
+    });
+  }
+
+  const linkRegister = () => {
+    router.push({
+      pathname: "/register"
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={linkList} title="Listar Serviços"></Button>
+      <Button onPress={linkRegister} title="Cadastrar"></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
