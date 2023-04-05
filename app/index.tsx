b9a2f06524c54a0af1dbe6228148749f20a1ed03
Jacson <jacsonsavio7@gmail.com>
2023-04-05 15:32:38 -0300

    bug do Header.tsx arrumado

--- app/index.tsx
+++ app/index.tsx
@@ -21,7 +21,7 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <Header title="ORDENS DE SERVIÇO" />
+      <Header />
 
       <DatePickerApp />
 
