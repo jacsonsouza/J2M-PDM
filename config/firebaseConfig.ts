75d577e2b3086a3aa3b40acfe52687534431a4ca
Jacson Souza <111143365+jacsonsouza@users.noreply.github.com>
2023-06-21 00:01:29 -0300

    Add files via upload

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,13 @@
+import { FirebaseOptions } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyDDkCyrd4mmxt1K5jMBpYY6avxMciW8_dc",
+  authDomain: "j2m-project.firebaseapp.com",
+  projectId: "j2m-project",
+  storageBucket: "j2m-project.appspot.com",
+  messagingSenderId: "537447824615",
+  appId: "1:537447824615:web:a4969b2e67f4645f12b642",
+};
+
+export default firebaseConfig;
